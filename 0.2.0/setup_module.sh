84cdcdadd763adee50651babbf9beea076816baa
Michael Mell <michael.mell@unibas.ch>
2023-08-01 17:17:50 +0200

    WIP: Start work to add usage of singularity container in module.

--- 0.2.0/setup_module.sh
+++ 0.2.0/setup_module.sh
@@ -2,6 +2,8 @@ setup_docker_container() {
   ### TODO: Check if an image with this tag already exists; if it does do not pull new one; this will avoid having to run the lengthy hash calculation
   ### michaelmell/mmpreprocesspy:v0.2.0
 
+  printf "Setting up module with Singularity container."
+
   docker pull "${CONTAINER_TAG}"
   id=$(docker create "${CONTAINER_TAG}")
 
@@ -13,6 +15,26 @@ setup_docker_container() {
   if [[ ! -f "${MOMA_BIN_DIRECTORY}/moma_preprocess.sh" ]]; then
     docker cp "${id}":"${HOST_SCRIPT_DIR}/moma_preprocess" "${MOMA_BIN_DIRECTORY}/moma_preprocess.sh"
   fi
+}
+
+function setup_singularity_container() {
+    printf "Setting up module with Singularity container."
+    SINGULARITY_CONTAINER_NAME="${CONTAINER_NAME//\//_}"
+#    echo "SINGULARITY_CONTAINER_NAME: ${SINGULARITY_CONTAINER_NAME}"
+    SINGULARITY_CONTAINER_FILENAME="${CONTAINER_NAME//\//_}_${VERSION}.sif"
+#    echo "SINGULARITY_CONTAINER_FILENAME: ${SINGULARITY_CONTAINER_FILENAME}"
+    SINGULARITY_CONTAINER_FILE_PATH="${SINGULARITY_CONTAINER_DIR}/${SINGULARITY_CONTAINER_FILENAME}"
+
+    if [[ ! -f "${SINGULARITY_CONTAINER_FILE_PATH}" ]]; then
+      singularity pull "${SINGULARITY_CONTAINER_FILE_PATH}" "docker://${CONTAINER_TAG}"
+    fi
+
+    if [[ ! -f "${MOMA_BIN_DIRECTORY}/mm_dispatch_preprocessing.sh" ]]; then
+      singularity  exec "${SINGULARITY_CONTAINER_FILE_PATH}" cp "${HOST_SCRIPT_DIR}/mm_dispatch_preprocessing.sh" .
+    fi
+    if [[ ! -f "${MOMA_BIN_DIRECTORY}/moma_preprocess.sh" ]]; then
+      singularity  exec "${SINGULARITY_CONTAINER_FILE_PATH}" cp "${HOST_SCRIPT_DIR}/moma_preprocess.sh" .
+    fi
 }
 
 setup_module() {
@@ -29,5 +51,14 @@ setup_module() {
     mkdir -p "${MOMA_BIN_DIRECTORY}"
   fi
 
-  setup_docker_container
+  if command -v docker &> /dev/null
+  then
+    setup_docker_container
+  elif command -v singularity &> /dev/null
+  then
+    setup_singularity_container
+  else
+      printf "ERROR: No container engine was found. Check that Singularity or Docker are correctly configured."
+      exit 1
+  fi
 }
