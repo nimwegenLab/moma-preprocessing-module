f34205dbf5683ea2692a31c3bdb2fa948a0e75b0
Michael Mell <michael.mell@unibas.ch>
2023-08-01 17:07:19 +0200

    Refactor docker setup to separate function to prepare for adding singularity container setup.

--- 0.2.0/setup_module.sh
+++ 0.2.0/setup_module.sh
@@ -1,3 +1,19 @@
+setup_docker_container() {
+  ### TODO: Check if an image with this tag already exists; if it does do not pull new one; this will avoid having to run the lengthy hash calculation
+  ### michaelmell/mmpreprocesspy:v0.2.0
+
+  docker pull "${CONTAINER_TAG}"
+  id=$(docker create "${CONTAINER_TAG}")
+
+  ### Copy support scripts from container to host
+  HOST_SCRIPT_DIR="/host_scripts"
+  if [[ ! -f "${MOMA_BIN_DIRECTORY}/mm_dispatch_preprocessing.sh" ]]; then
+    docker cp "${id}":"${HOST_SCRIPT_DIR}/mm_dispatch_preprocessing.sh" "${MOMA_BIN_DIRECTORY}/mm_dispatch_preprocessing.sh"
+  fi
+  if [[ ! -f "${MOMA_BIN_DIRECTORY}/moma_preprocess.sh" ]]; then
+    docker cp "${id}":"${HOST_SCRIPT_DIR}/moma_preprocess" "${MOMA_BIN_DIRECTORY}/moma_preprocess.sh"
+  fi
+}
 
 setup_module() {
   export VERSION="v0.2.0"
@@ -13,18 +29,5 @@ setup_module() {
     mkdir -p "${MOMA_BIN_DIRECTORY}"
   fi
 
-  ### TODO: Check if an image with this tag already exists; if it does do not pull new one; this will avoid having to run the lengthy hash calculation
-  ### michaelmell/mmpreprocesspy:v0.2.0
-
-  docker pull "${CONTAINER_TAG}"
-  id=$(docker create "${CONTAINER_TAG}")
-
-  ### Copy support scripts from container to host
-  HOST_SCRIPT_DIR="/host_scripts"
-  if [[ ! -f "${MOMA_BIN_DIRECTORY}/mm_dispatch_preprocessing.sh" ]]; then
-    docker cp "${id}":"${HOST_SCRIPT_DIR}/mm_dispatch_preprocessing.sh" "${MOMA_BIN_DIRECTORY}/mm_dispatch_preprocessing.sh"
-  fi
-  if [[ ! -f "${MOMA_BIN_DIRECTORY}/moma_preprocess.sh" ]]; then
-    docker cp "${id}":"${HOST_SCRIPT_DIR}/moma_preprocess" "${MOMA_BIN_DIRECTORY}/moma_preprocess.sh"
-  fi
+  setup_docker_container
 }
