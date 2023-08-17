43a1e315f766ec0fc47c88fbfd90ce8fb454eca8
Michael Mell <michael.mell@unibas.ch>
2023-08-17 15:26:10 +0200

    Adapt scripts to the new Python wrapper script.

--- 0.2.0/setup_module.sh
+++ 0.2.0/setup_module.sh
@@ -1,10 +1,8 @@
 setup_docker_container() {
-  ### TODO: Check if an image with this tag already exists; if it does do not pull new one; this will avoid having to run the lengthy hash calculation
-  ### michaelmell/mmpreprocesspy:v0.2.0
-
-  printf "Setting up module with Singularity container.\n"
-
-  docker pull "${CONTAINER_TAG}"
+  if [[ "$(docker images -q "${CONTAINER_TAG}" 2> /dev/null)" == "" ]]; then
+    printf "Setting up module with Docker container.\n"
+    docker pull "${CONTAINER_TAG}"
+  fi
   id=$(docker create "${CONTAINER_TAG}")
 
   ### Copy support scripts from container to host
@@ -12,40 +10,38 @@ setup_docker_container() {
   if [[ ! -f "${MOMA_BIN_DIRECTORY}/mm_dispatch_preprocessing.sh" ]]; then
     docker cp "${id}":"${HOST_SCRIPT_DIR}/mm_dispatch_preprocessing.sh" "${MOMA_BIN_DIRECTORY}/mm_dispatch_preprocessing.sh"
   fi
-  if [[ ! -f "${MOMA_BIN_DIRECTORY}/moma_preprocess.sh" ]]; then
-    docker cp "${id}":"${HOST_SCRIPT_DIR}/moma_preprocess" "${MOMA_BIN_DIRECTORY}/moma_preprocess.sh"
+  if [[ ! -f "${MOMA_BIN_DIRECTORY}/moma_preprocess" ]]; then
+    docker cp "${id}":"${HOST_SCRIPT_DIR}/moma_preprocess" "${MOMA_BIN_DIRECTORY}/moma_preprocess"
   fi
 }
 
 function setup_singularity_container() {
-    printf "Setting up module with Singularity container.\n"
-    SINGULARITY_CONTAINER_NAME="${CONTAINER_NAME//\//_}"
-#    echo "SINGULARITY_CONTAINER_NAME: ${SINGULARITY_CONTAINER_NAME}"
-    SINGULARITY_CONTAINER_FILENAME="${CONTAINER_NAME//\//_}_${VERSION}.sif"
-#    echo "SINGULARITY_CONTAINER_FILENAME: ${SINGULARITY_CONTAINER_FILENAME}"
-    SINGULARITY_CONTAINER_FILE_PATH="${SINGULARITY_CONTAINER_DIR}/${SINGULARITY_CONTAINER_FILENAME}"
-
     if [[ ! -f "${SINGULARITY_CONTAINER_FILE_PATH}" ]]; then
+      printf "Setting up module with Singularity container.\n"
       singularity pull "${SINGULARITY_CONTAINER_FILE_PATH}" "docker://${CONTAINER_TAG}"
     fi
 
+    ### Copy support scripts from container to host
     HOST_SCRIPT_DIR="/host_scripts"
     if [[ ! -f "${MOMA_BIN_DIRECTORY}/mm_dispatch_preprocessing.sh" ]]; then
       singularity  exec --bind "${SINGULARITY_CONTAINER_DIR}":"${SINGULARITY_CONTAINER_DIR}" "${SINGULARITY_CONTAINER_FILE_PATH}" cp "${HOST_SCRIPT_DIR}/mm_dispatch_preprocessing.sh" "${SINGULARITY_CONTAINER_DIR}/mm_dispatch_preprocessing.sh"
     fi
-    if [[ ! -f "${MOMA_BIN_DIRECTORY}/moma_preprocess.sh" ]]; then
-      singularity  exec --bind "${SINGULARITY_CONTAINER_DIR}":"${SINGULARITY_CONTAINER_DIR}" "${SINGULARITY_CONTAINER_FILE_PATH}" cp "${HOST_SCRIPT_DIR}/moma_preprocess" "${SINGULARITY_CONTAINER_DIR}/moma_preprocess.sh"
+    if [[ ! -f "${MOMA_BIN_DIRECTORY}/moma_preprocess" ]]; then
+      singularity  exec --bind "${SINGULARITY_CONTAINER_DIR}":"${SINGULARITY_CONTAINER_DIR}" "${SINGULARITY_CONTAINER_FILE_PATH}" cp "${HOST_SCRIPT_DIR}/moma_preprocess" "${SINGULARITY_CONTAINER_DIR}/moma_preprocess"
     fi
 }
 
 setup_module() {
   export VERSION="v0.2.0"
-  export CONTAINER_NAME="michaelmell/mmpreprocesspy"
-  export CONTAINER_TAG="${CONTAINER_NAME}:${VERSION}"
+  export CONTAINER_NAME="mmpreprocesspy"
+  CONTAINER_NAMESPACE="nimwegenlab"
+  export CONTAINER_TAG="${CONTAINER_NAMESPACE}/${CONTAINER_NAME}:${VERSION}"
 
   DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
   MOMA_BIN_DIRECTORY="${DIR}/bin/"
   export SINGULARITY_CONTAINER_DIR="${MOMA_BIN_DIRECTORY}"
+  SINGULARITY_CONTAINER_FILENAME="${CONTAINER_NAME//\//_}_${VERSION}.sif"
+  export SINGULARITY_CONTAINER_FILE_PATH="${SINGULARITY_CONTAINER_DIR}/${SINGULARITY_CONTAINER_FILENAME}"
 
   if [[ ! -d "${MOMA_BIN_DIRECTORY}" ]]
   then
